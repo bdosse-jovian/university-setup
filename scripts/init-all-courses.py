fbd746ff3821ef954a5118a248c00084b1dd7c68
Benjamin Dosse <bdosse@student.uliege.be>
2022-11-25 21:15:26 +0100

    Added some basic data to master.tex for each course.

--- scripts/init-all-courses.py
+++ scripts/init-all-courses.py
@@ -4,14 +4,25 @@ from courses import Courses
 for course in Courses():
     lectures = course.lectures
     course_title = lectures.course.info["title"]
-    lines = [r'\documentclass[a4paper]{article}',
-             r'\input{~/uni/latex/header.tex}',
-             fr'\title{{{course_title}}}',
+    lines = [r'\documentclass[a4paper, 11pt]{article}',
+             r'\input{../../latex/header/header.tex}'
+             r'\input{../../latex/header/article.tex}',
+             r'% \input{../../latex/header/book.tex}',
+             r'\makeatletter',
+             fr'\title{{{course_title}}}\let\thetitle\@title',
+             r'\date{\textsc{Année Académique 2022--2023}}\let\thedate\@date',
+             r'% \author{\emph{Notes de cours par} Benjamin Dosse}\let\theauthor\@author',
+             r'\makeatother',
              r'\begin{document}',
+             r'% \frontmatter',
              r'\maketitle',
              r'\tableofcontents',
+             r'% \mainmatter',
              fr'% start lectures',
              fr'% end lectures',
+             r'\printbibliography',
+             r'\addcontentsline{toc}{section}{Références}',
+             r'% \addcontentsline{toc}{chapter}{Bibliographie}',
              r'\end{document}']
     lectures.master_file.touch()
     lectures.master_file.write_text('\n'.join(lines))
